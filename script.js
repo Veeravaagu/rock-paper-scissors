9a2a10a7de81af33292bcbc533ba5cfbd181eaea
“Veeravaagu” <93789055+Veeravaagu@users.noreply.github.com>
2023-11-08 14:30:59 +0530

    Add a Scorecard Tally and Update the score every time except a tie

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+// ------->>>>>>>>>>>>>>Initialized variables
+
 let getPlayerChoice;
 
 let playerCount = 0;
 
 let computerCount = 0;
+
+// ------->>>>>>>>>>>>>>Reference DOMs
+
+let playerDisplay = document.querySelector(".playerDisplay");
+
+let computerDisplay = document.querySelector(".computerDisplay");
 
 let rock = document.querySelector(".Rock");
 
@@ -12,6 +20,8 @@ let scissors = document.querySelector(".Scissors");
 
 let restart = document.querySelector(".Restart");
 
+// ------->>>>>>>>>>>>>>EventListeners
+
 restart.addEventListener('click',isRestart);
 
 rock.addEventListener('click', isRock);
@@ -20,8 +30,10 @@ paper.addEventListener('click', isPaper);
 
 scissors.addEventListener('click', isScissors);
 
+
 alert("\nThe First One to Reach Five Wins!!!! \n\nGood Luck!")
 
+// ------->>>>>>>>>>>>>>Callback functions to EventListeners
 function isRock () {
     getPlayerChoice = "rock";
     
@@ -40,9 +52,17 @@ function isScissors () {
     playRound()
 }
 
-function isRestart() 
- {   console.log (` score is ${playerCount} to ${computerCount}`); 
-                            playerCount = 0; computerCount = 0;}
+function isRestart() {
+    
+    playerCount = 0; 
+    
+    computerCount = 0;
+    
+    playerDisplay.textContent = playerCount;
+       
+    computerDisplay.textContent = computerCount;
+    
+}
 
 
 function getComputerChoice() 
@@ -75,26 +95,31 @@ function playRound(playerSelection,computerSelection)
 
 {
 
+    // See Callback Functions isRock,isPaper,isScissors.
     playerSelection = getPlayerChoice;
 
 
     computerSelection = getComputerChoice();
 
-    let result;
 
     if ( playerCount === 5 || computerCount === 5) 
     
     {
-       
+    //    alert is there for when the user clicks any three buttons accidentally for the sixth time
        alert("would you like to restart?");
+       
        isRestart()
+    //    Update the Scorecard Tally
+       playerDisplay.textContent = playerCount;
+       
+       computerDisplay.textContent = computerCount;
     
     }
     
     else
     
     {
-          // condition for a tie
+        // condition for a tie
           if (playerSelection === computerSelection) 
     
           {
@@ -103,7 +128,7 @@ function playRound(playerSelection,computerSelection)
       
   
           }
-      // condition for player to win 
+        // condition for player to win 
           else if ( 
           
               (playerSelection === "rock" && computerSelection === "scissors") || 
@@ -114,15 +139,29 @@ function playRound(playerSelection,computerSelection)
       
           {
               playerCount += 1
+              
+              
 
-              if (playerCount === 5){
+                if (playerCount === 5){
+                
+                playerDisplay.textContent = 5;
+
+                playerDisplay.style.color = "Green"
+
+                computerDisplay.style.color = "Red"
+                
                 alert(`You Win, the score is ${playerCount} to ${computerCount}`); 
+
            }
+                else {
+           
+                    playerDisplay.textContent = playerCount;
+                }
   
            
   
           }
-      // condition for computer to win
+            // condition for computer to win
           else if ( 
           
               (computerSelection === "rock" && playerSelection === "scissors") || 
@@ -134,9 +173,23 @@ function playRound(playerSelection,computerSelection)
           {
               computerCount += 1;
 
-              if (computerCount === 5){
+                if (computerCount === 5){
+
+                computerDisplay.textContent = 5;
+
+                computerDisplay.style.color = "Green"
+
+                playerDisplay.style.color = "Red"
+                
+                
                 alert(`You Lose, the score is ${playerCount} to ${computerCount}`); 
+
            }
+                else{
+                    
+                    computerDisplay.textContent = computerCount;
+                
+                }
   
             
   
@@ -152,7 +205,6 @@ function playRound(playerSelection,computerSelection)
           }
      
     }
-    console.log (`Your score is ${playerCount} and the Computer's score is ${computerCount}`);
     
     return
 
